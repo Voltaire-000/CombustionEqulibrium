5ac48bfb9d35da66b99b10834e5028f8a0565915
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-25 19:49:35 +0700

    test filter on shortThermo

--- ChemicalEquConsole/Program.cs
+++ ChemicalEquConsole/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace ChemicalEquConsole
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using Newtonsoft.Json.Linq;
+
+string json = File.ReadAllText("shortThermo.json");
+JObject jObj = JObject.Parse(json);
+
+// define filter parameter as a lambda expression
+Func<JToken, bool> filter = r => (string)r["reactant"] == "ALC";
+
+// apply the filter to the JObject and get the filtered records as a JArray
+JArray filtered = new JArray(jObj["shortThermo"].Where(filter));
+
+Console.WriteLine(filtered);
+
+Console.WriteLine("Hello, World!");
