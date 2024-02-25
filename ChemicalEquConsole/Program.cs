1821bb76c257945b6ee326ad31132dabe254a333
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-25 20:30:14 +0700

    working filters

--- ChemicalEquConsole/Program.cs
+++ ChemicalEquConsole/Program.cs
@@ -1,13 +1,18 @@
 ﻿using Newtonsoft.Json.Linq;
 
-string json = File.ReadAllText("shortThermo.json");
+//string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "shortThermo.json");
+string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "thermoINPjson.json");
+
+string json = File.ReadAllText(path);
 JObject jObj = JObject.Parse(json);
 
 // define filter parameter as a lambda expression
-Func<JToken, bool> filter = r => (string)r["reactant"] == "ALC";
+//Func<JToken, bool> filter = r => (string)r["reactant"] == "ALC";
+//Func<JToken, bool> filter = r => (bool)r[key: "gaseous"] == false;
+//Func<JToken, bool> filter = r => (double)r[key: "molecularWeight"] <= 25.0;
 
 // apply the filter to the JObject and get the filtered records as a JArray
-JArray filtered = new JArray(jObj["shortThermo"].Where(filter));
+JArray filtered = new JArray(jObj["thermo"].Where(filter));
 
 Console.WriteLine(filtered);
 
